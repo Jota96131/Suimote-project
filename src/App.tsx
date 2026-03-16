ac8f10180125d8fb2b8eff3ed72e476a437ca87d
Gushiken Jota <j13119613@gmail.com>
2026-03-16 15:53:02 +0900

    認証機能を削除

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,16 @@ import RecordsPage from "./pages/RecordsPage";
 import RecordDetailPage from "./pages/RecordDetailPage";
 import AddRecordPage from "./pages/AddRecordPage";
 import ProfilePage from "./pages/ProfilePage";
-import AuthPage from "./pages/AuthPage";
-import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/records" element={<PrivateRoute><RecordsPage /></PrivateRoute>} />
-        <Route path="/records/new" element={<PrivateRoute><AddRecordPage /></PrivateRoute>} />
-        <Route path="/records/:id" element={<PrivateRoute><RecordDetailPage /></PrivateRoute>} />
-        <Route path="/profile" element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
+        <Route path="/records" element={<RecordsPage />} />
+        <Route path="/records/new" element={<AddRecordPage />} />
+        <Route path="/records/:id" element={<RecordDetailPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </BrowserRouter>
   );
